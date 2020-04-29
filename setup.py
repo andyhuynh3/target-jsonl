13edd46162f473faced1468eb099c983399408d2
Andy Huynh <andy.huynh312@gmail.com>
2020-04-29 16:13:52 -0700

    Update setup

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
-
 from setuptools import setup
 
-setup(name='target-jsonl',
-      version='0.1.0',
-      description='Singer.io target for writing JSON Line files',
-      author='Andy Huynh',
-      url='https://singer.io',
-      classifiers=['Programming Language :: Python :: 3 :: Only'],
-      py_modules=['target_jsonl'],
-      install_requires=[
-          'jsonschema==2.6.0',
-          'singer-python==2.1.4',
-      ],
-      entry_points='''
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name='target-jsonl',
+    version='0.1.1',
+    description='Singer.io target for writing JSON Line files',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Andy Huynh',
+    author_email="andy.huynh312@gmail.com",
+    url="https://github.com/andyh1203/target-jsonl",
+    keywords=["singer", "singer.io", "target", "etl"],
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    py_modules=['target_jsonl'],
+    install_requires=['jsonschema==2.6.0', 'singer-python==2.1.4'],
+    entry_points='''
           [console_scripts]
           target-jsonl=target_jsonl:main
       ''',
