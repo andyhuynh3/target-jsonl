6318bda21173b81901a60ae7749d3e5a660aa27b
Ken Payne <ken@meltano.com>
2022-11-28 13:34:22 +0000

    shim setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python
-from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as f:
-    readme = f.read()
+import setuptools
 
-setup(
-    name='target-jsonl',
-    version='0.1.4',
-    description='Singer.io target for writing JSON Line files',
-    long_description=readme,
-    long_description_content_type='text/markdown',
-    author='Andy Huynh',
-    author_email="andy.huynh312@gmail.com",
-    url="https://github.com/andyh1203/target-jsonl",
-    keywords=["singer", "singer.io", "target", "etl"],
-    classifiers=['Programming Language :: Python :: 3 :: Only'],
-    py_modules=['target_jsonl'],
-    install_requires=['jsonschema==2.6.0', 'singer-python==5.8.0', 'adjust-precision-for-schema==0.3.3'],
-    entry_points='''
-          [console_scripts]
-          target-jsonl=target_jsonl:main
-      ''',
-)
+if __name__ == "__main__":
+    setuptools.setup()
